dc36bfd24902d362a209b6add8795b68299a0a01
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-14 15:35:20 -0400

    dueler

---
+++ StephanieYu/src/duel/CharacterB.java
@@ -0,0 +1,15 @@
+package duel;
+
+public class CharacterB implements Dueler 
+{
+	public CharacterB() {
+	}
+		public void taunt() {
+			String taunt = ("yo mama so fat");
+			System.out.println(taunt);
+		}
+		public String getName() {
+			String name = ("Carlos");
+			return name;
+		}
+}
