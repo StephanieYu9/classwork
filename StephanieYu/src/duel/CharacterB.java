4b1ba5ec364cc1c61dc08552a15c6ab945439a39
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-25 15:26:38 -0400

    actions

--- StephanieYu/src/duel/CharacterB.java
+++ StephanieYu/src/duel/CharacterB.java
@@ -2,7 +2,9 @@ package duel;
 
 public class CharacterB implements Dueler
 {
-	private int hp = 100; 
+	private int hp; 
+	private boolean isLoaded = false; 
+	private int lastMove = 0;
 	
 	public CharacterB() {
 	}
@@ -21,14 +23,29 @@ public class CharacterB implements Dueler
 			return hp;
 		}
 		public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-			if (d.getHP() == 100) {
+			if (d.getHP() == hp) {
 				return true;
 			}
 			return false;
 		}
 		public int getAction(Object caller) {
 			if(caller instanceof Duel) {
-				
+				if(isLoaded == false) {
+					return Duel.LOADING;
+				}
+				isLoaded = true;
+				lastMove = 0;
+				}
+			else {
+				if(isLoaded == true && lastMove == 0) {
+						return Duel.GUARDING;
+					}
+					lastMove = 2;
+				if (isLoaded == true && lastMove == 2) {
+					return Duel.SHOOTING
+				}
+				isLoaded = false;
+				lastMove = 1;
 			}
 			
 		}
