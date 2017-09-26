3181dab2c49b30a8d92078d0bac4b9a0d9a003d7
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-26 15:31:41 -0400

    attendance

---
+++ StephanieYu/src/attendance/Student.java
@@ -0,0 +1,12 @@
+package attendance;
+
+public class Student implements Attendee {
+	
+	private String first;
+	private String last;
+	
+	public Student(String first, String last) {
+		this.firstName 
+	}
+	
+}
