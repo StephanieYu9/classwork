a914649a99cc0eaae16d6940af7dd058b0d2bf9a
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-27 14:26:12 -0400

    attenance

--- StephanieYu/src/attendance/Student.java
+++ StephanieYu/src/attendance/Student.java
@@ -2,11 +2,43 @@ package attendance;
 
 public class Student implements Attendee {
 	
-	private String first;
-	private String last;
+	private String firstName;
+	private String lastName;
+	private boolean present;
 	
-	public Student(String first, String last) {
-		this.firstName 
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName= lastName;
+		return;
 	}
-	
+	public boolean isPresent(){
+		return present;
+	}
+	public void setPresent(boolean present) {
+		this.present = present;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public boolean matches(String first, String last) {
+		return first.toLowerCase().equals(firstName.toLowerCase()) && last.toLowerCase().equals(lastName.toLowerCase());
+	}
+	public boolean matches(String last) {
+		return last.toLowerCase().equals(lastName.toLowerCase());
+	}
+	public String getReportString() {
+		String report = lastName;
+		report = restrictStringToLength(report,20);
+		report+=firstName;
+		report = restrictStringToLength(report, 40);
+		if(present) {
+			report+="PRESENT\n";
+		}else 
+		{
+			report+="ABSENT\n";
+		}
+	}
 }
