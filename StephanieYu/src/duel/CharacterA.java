71ee566996af6d52a790e6e998b12b239596c830
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-27 14:31:25 -0400

    character A

---
+++ StephanieYu/src/duel/CharacterA.java
@@ -0,0 +1,84 @@
+package duel;
+
+import duel.Duel;
+
+public class CharacterA extends Duel implements Dueler 
+{
+	private String name;
+	private int hp;
+	private boolean loadedGun = false;
+	
+	public void taunt() 
+	{
+		String taunt = ("Git gud Karluz");
+		System.out.println(taunt);
+	}
+	public String getName()
+	{
+		name = "Chaddd";
+		return name;
+	}
+	public void setStartingHP(int hp)
+	{
+		hp = 
+	}
+	public int getHP()
+	{
+		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp)
+	{
+		if (d.getHP() == hp)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public int getAction(Object caller)
+	{
+		if(caller instanceof Duel)
+		{
+			if (loadedGun == true)
+			{
+				if (Math.random() > 0.4)
+				{
+					return Duel.SHOOTING;
+					loadedGun = false;
+				}
+				else
+				{
+					if (Math.random() < 0.5)
+					{
+						return Duel.GUARDING;
+					}
+				}
+			}
+			else
+			{
+				if (Math.random() > 0.2)
+				{
+					return Duel.GUARDING;
+				}
+				else
+				{
+					return Duel.LOADING;
+					loadedGun = true;
+				}
+			}
+		}
+		else
+		{
+			return YEAH_RIGHT;
+		}
+	}
+	public void hit(Object caller)
+	{
+		if (caller instanceof Duel)
+		{
+			hp = hp - 10;
+		}
+	}
+}
