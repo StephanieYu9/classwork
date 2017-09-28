c05681a7e3fa85ce07c501c654ddf67fd817b3b5
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-28 15:33:08 -0400

    aa

--- StephanieYu/src/chatbot/Chatbot.java
+++ StephanieYu/src/chatbot/Chatbot.java
@@ -2,8 +2,30 @@ package chatbot;
 
 public class Chatbot {
 
+	private String username;
+	private boolean chatting;
+	private Topic stephanie;
+	
 	public Chatbot() {
-		// TODO Auto-generated constructor stub
+		stephanie = new ChatbotStephanie();
+		username = "Unknown User";
+		chatting = true;
+	}
+
+	public void startChatting() {
+		//whenever you print or get input, use these methods 
+		ChatbotMain.print("Hi! I am an intellegent machine that can respond to your input. Tell me your name.");
+		username = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What would you like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(stephanie.isTriggered(response)) {
+				chatting = false; //exit the while loop. IMPORTANT.
+				stephanie.talk(response);
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand. I never said I was perfect.");
+			}
+		}
 	}
 
 }
