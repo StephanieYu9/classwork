38beeac43d4cded364cf476996513be475d7b0f5
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-28 15:01:27 -0400

    chat

--- StephanieYu/src/chatbot/ChatbotMain.java
+++ StephanieYu/src/chatbot/ChatbotMain.java
@@ -2,6 +2,8 @@ package chatbot;
 
 public class ChatbotMain {
 
+	public static final Chatbot chatbot = new Chatbot();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
